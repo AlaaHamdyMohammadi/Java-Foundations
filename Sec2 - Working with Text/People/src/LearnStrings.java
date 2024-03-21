0df3b96585856ca83968e07bddab788bb8bf8133
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-21 18:27:05 +0200

    People project

---
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -0,0 +1,7 @@
+public class LearnStrings {
+    public static void main(String[] args) {
+        String fruit1 = "appla";
+        String fruit2 = "appla";
+        System.out.println(fruit1 == fruit2);
+    }
+}
