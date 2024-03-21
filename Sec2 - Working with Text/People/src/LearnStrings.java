6bd7df76c640cb8abe9d93e85ed6b8708b7e6f39
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-22 00:31:35 +0200

    replace method

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,9 +1,7 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String text1 = "";
-       String text2 = " ";
-       System.out.println(text1.isEmpty());
-        System.out.println(text2.isBlank());
-//       System.out.println("Text2 = " + text2.toLowerCase());
+       String text = "Hello new C++ course";
+
+       System.out.println(text.replace("C++", "Java"));
     }
 }
