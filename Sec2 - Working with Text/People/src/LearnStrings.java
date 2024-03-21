dc13da20e1d137b935729fd3bc62287eeab8e2a4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-22 00:21:27 +0200

    isEmpty and isBlank

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,8 +1,9 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String text1 = "abcdfg";
-       String text2 = "ABCDEFG";
-       System.out.println("Text1 = " + text1.toUpperCase());
-       System.out.println("Text2 = " + text2.toLowerCase());
+       String text1 = "";
+       String text2 = " ";
+       System.out.println(text1.isEmpty());
+        System.out.println(text2.isBlank());
+//       System.out.println("Text2 = " + text2.toLowerCase());
     }
 }
