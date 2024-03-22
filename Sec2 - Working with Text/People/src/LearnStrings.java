b270fa111e1eeb90bb28e8991f5f834f98290891
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-22 16:03:43 +0200

    compareTo

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,14 +1,14 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String firstName = "  Alaa  ";
+//       String firstWord = "Apple";
+//       String secondWord = "Banana";
+//        System.out.println(firstWord.compareTo(secondWord));
 
-       System.out.format("'%s'", firstName.strip());
-       System.out.println("");
-       System.out.format("'%s'", firstName.stripLeading());
-       System.out.println("");
-       System.out.format("'%s'", firstName.stripTrailing());
-       System.out.println("");
-
-
+        System.out.println(compareToIgnoreCase("Alaa", "arwa"));
+    }
+    public static int compareToIgnoreCase(String text1, String text2){
+         String firstText = text1.toLowerCase();
+         String secondText = text2.toLowerCase();
+         return firstText.compareTo(secondText);
     }
 }
