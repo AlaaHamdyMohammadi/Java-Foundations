417df1e6924448aabaca44f09977fd87e079a695
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-22 23:37:22 +0200

    concat

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,14 +1,7 @@
 public class LearnStrings {
     public static void main(String[] args) {
-//       String firstWord = "Apple";
-//       String secondWord = "Banana";
-//        System.out.println(firstWord.compareTo(secondWord));
-
-        System.out.println(compareToIgnoreCase("Alaa", "arwa"));
-    }
-    public static int compareToIgnoreCase(String text1, String text2){
-         String firstText = text1.toLowerCase();
-         String secondText = text2.toLowerCase();
-         return firstText.compareTo(secondText);
+       String firstWord = "Apple";
+       String secondWord = "Banana";
+       System.out.println(firstWord.concat(" ".concat(secondWord)));
     }
 }
