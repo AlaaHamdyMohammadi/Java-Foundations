5ab7b5a7383c70cfea8da2746b7c89e43b60c357
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-23 17:54:05 +0200

    split

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,28 +1,18 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String phoneNumber = "    (234) 333-5551".strip();
-       String areaCode = parseAreaCode(phoneNumber);
-       String exchange = parseExchange(phoneNumber);
-       String lineNumber = parseLineNumber(phoneNumber);
-
-       System.out.println(areaCode);
-       System.out.println(exchange);
-       System.out.println(lineNumber);
+       String text = """
+               Alaa, Hamdy, Mohammadi
+               Alaa, Hamdy, Mohammadi
+               Alaa, Hamdy, Mohammadi
+               Alaa, Hamdy, Mohammadi
+               Alaa, Hamdy, Mohammadi
+               """;
+       String[] me = text.split("\n");
+       System.out.println(me.length);
+       String[] meItems = me[2].split(", ");
+        System.out.println(meItems[0]);
     }
 
-    public static String parseAreaCode(String phoneNumber){
-        int openParens = phoneNumber.indexOf("(");
-        int closeParens = phoneNumber.indexOf(")");
-        return phoneNumber.substring(openParens + 1, closeParens);
-    }
-    public static String parseExchange(String phoneNumber){
-        int space = phoneNumber.indexOf(" ");
-        int dash = phoneNumber.indexOf("-");
-        return phoneNumber.substring(space + 1, dash);
-    }
-    public static String parseLineNumber(String phoneNumber){
-        int dash = phoneNumber.indexOf("-");
-        return phoneNumber.substring(dash + 1);
-    }
+
 
 }
