1e72be1df21cb533378a1402cd1ce2fc99ec431e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-23 17:17:10 +0200

    indexOf

--- Sec2 - Working with Text/People/src/LearnStrings.java
+++ Sec2 - Working with Text/People/src/LearnStrings.java
@@ -1,7 +1,27 @@
 public class LearnStrings {
     public static void main(String[] args) {
-       String firstWord = "Apple";
-       String secondWord = "Banana";
-       System.out.println(firstWord.concat(" ".concat(secondWord)));
+       String phoneNumber = "(234) 333-5551";
+       String areaCode = parseAreaCode(phoneNumber);
+       String exchange = parseExchange(phoneNumber);
+       String lineNumber = parseLineNumber(phoneNumber);
+
+       System.out.println(areaCode);
+       System.out.println(exchange);
+       System.out.println(lineNumber);
+    }
+
+    public static String parseAreaCode(String phoneNumber){
+        int openParens = phoneNumber.indexOf("(");
+        int closeParens = phoneNumber.indexOf(")");
+        return phoneNumber.substring(openParens + 1, closeParens);
+    }
+    public static String parseExchange(String phoneNumber){
+        int space = phoneNumber.indexOf(" ");
+        int dash = phoneNumber.indexOf("-");
+        return phoneNumber.substring(space + 1, dash);
+    }
+    public static String parseLineNumber(String phoneNumber){
+        int dash = phoneNumber.indexOf("-");
+        return phoneNumber.substring(dash + 1);
     }
 }
