acf8520d770fd7241a712c134a9f9c5620edc133
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-24 02:28:38 +0200

    Part1

--- Sec4 - Regular Expressions/People/src/RegexPractice.java
+++ Sec4 - Regular Expressions/People/src/RegexPractice.java
@@ -1,2 +1,6 @@
 public class RegexPractice {
+    public static void main(String[] args) {
+        System.out.println("gat".matches("[a-fA-F]at"));
+        System.out.println("Bat".matches("[^a-f]at"));
+    }
 }
