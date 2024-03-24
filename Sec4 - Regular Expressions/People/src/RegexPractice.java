abb0a0b9975424497bd4a383148975f2c803e2c6
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-24 06:15:45 +0200

    Part2

--- Sec4 - Regular Expressions/People/src/RegexPractice.java
+++ Sec4 - Regular Expressions/People/src/RegexPractice.java
@@ -1,6 +1,9 @@
 public class RegexPractice {
     public static void main(String[] args) {
-        System.out.println("gat".matches("[a-fA-F]at"));
-        System.out.println("Bat".matches("[^a-f]at"));
+        System.out.println("gat".matches("[a-fA-F]at")); // Contain
+        System.out.println("Bat".matches("[^a-f]at")); // Not contain
+        System.out.println("wat".matches("\\wat")); // String OR Numbers OR Underscore
+        System.out.println("5at".matches("\\dat")); // Only numbers
+        System.out.println("321.333  7652".matches("\\d{3}[-.\\s]+\\d{3}[-.\\s]+\\d{4}")); // Only numbers
     }
 }
