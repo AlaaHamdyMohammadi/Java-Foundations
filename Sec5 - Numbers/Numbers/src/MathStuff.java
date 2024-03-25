49149de1d77f215eb45a3ca0383856c21fbe829c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-25 17:04:33 +0200

    Math operations

--- Sec5 - Numbers/Numbers/src/MathStuff.java
+++ Sec5 - Numbers/Numbers/src/MathStuff.java
@@ -1,5 +1,7 @@
 public class MathStuff {
     public static void main(String[] args) {
-        System.out.println(7 / 4f);
+        int x = 1;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
