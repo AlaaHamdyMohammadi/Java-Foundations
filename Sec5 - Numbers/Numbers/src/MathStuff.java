d5e81e947b491825adb879c2200f3a92ef6b818e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-25 17:35:15 +0200

    Math functions

--- Sec5 - Numbers/Numbers/src/MathStuff.java
+++ Sec5 - Numbers/Numbers/src/MathStuff.java
@@ -1,7 +1,24 @@
 public class MathStuff {
     public static void main(String[] args) {
-        int x = 1;
-        System.out.println(++x);
-        System.out.println(x);
+
+        System.out.println(Math.abs(-5));
+        System.out.println(Math.ceil(5.1));
+        System.out.println(Math.floor(-5.7)); //-6.0
+        System.out.println(Math.floor(5.7)); //5.0
+        System.out.println(Math.floor(5.7)); //5.0
+
+        int num1 = 5, num2 = 3;
+        System.out.println("Min = " + Math.min(num1, num2));
+        System.out.println("Max = " + Math.max(num1, num2));
+        System.out.println("Power = " + Math.pow(num1, num2)); // 125.0 => pow return double
+
+        System.out.println((int)(Math.random() * 10));
+        System.out.println((int)(Math.random() * 10));
+        System.out.println((int)(Math.random() * 10));
+
+        System.out.println((Math.round(5.7))); //
+        System.out.println((Math.round(5.1)));
+
+
     }
 }
