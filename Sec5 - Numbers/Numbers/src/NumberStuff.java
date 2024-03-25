690348fc8e2bb9a71f5c872d7a91f85f7d64fe02
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-25 16:34:16 +0200

    Math operations

--- Sec5 - Numbers/Numbers/src/NumberStuff.java
+++ Sec5 - Numbers/Numbers/src/NumberStuff.java
@@ -1,2 +1,8 @@
 public class NumberStuff {
+    public static void main(String[] args) {
+        byte myByte = 127;
+
+        int anotherInt = 0b11; //binary
+        System.out.println(anotherInt);
+    }
 }
