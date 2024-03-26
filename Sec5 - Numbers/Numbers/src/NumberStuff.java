d8cebd8037b2dfffc64cd7e2add3d42855d50d2a
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-26 17:47:59 +0200

    Introducing BigDecimal

--- Sec5 - Numbers/Numbers/src/NumberStuff.java
+++ Sec5 - Numbers/Numbers/src/NumberStuff.java
@@ -1,10 +1,12 @@
-import java.util.Random;
+import java.math.BigDecimal;
 
 public class NumberStuff {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(10));
-        System.out.println(random.nextInt(10));
-        System.out.println(random.nextInt(10));
+        float num1 = 2.15f, num2 = 1.10f;
+
+        BigDecimal num3 = new BigDecimal("2.15");
+        BigDecimal num4 = new BigDecimal("1.10");
+        System.out.println(num1 - num2);
+        System.out.println(num3.subtract(num4)); // I can't do that (num3 - num4) because these ara non primitives
     }
 }
