4e145b0c0c0777f93dee8fe099668be08a9d3b24
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-27 18:09:31 +0200

    If else

--- Sec6- Control Flow/Control Flow/src/GuessIt.java
+++ Sec6- Control Flow/Control Flow/src/GuessIt.java
@@ -1,2 +1,17 @@
+import java.util.Random;
 public class GuessIt {
+    public static void main(String[] args) {
+        System.out.println(guessNumber(3));
+    }
+    public static int guessNumber(int number){
+        Random random = new Random();
+        int result = random.nextInt(number + 1);
+            System.out.printf("Your Guess = %d%n", result);
+        if(result == 3){
+            System.out.printf("Your Guess is right");
+        }else{
+            System.out.printf("Your Guess is wrong");
+        }
+        return result;
+    }
 }
