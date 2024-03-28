4aa99b7be417fd88d4b22fd4ef2a040ac8884e86
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-28 02:08:54 +0200

    While & Do While

--- Sec6- Control Flow/Control Flow/src/GuessIt.java
+++ Sec6- Control Flow/Control Flow/src/GuessIt.java
@@ -1,25 +1,15 @@
-import java.util.Random;
 public class GuessIt {
     public static void main(String[] args) {
-        System.out.println(guessNumber(3));
-    }
-    public static int guessNumber(int number){
-        Random random = new Random();
-        int result = random.nextInt(number + 1);
-        switch (result){
-            case 1:
-                System.out.println("The color is RED");
-                break;
-            case 2:
-                System.out.println("The color is yellow");
-                break;
-            case 3:
-                System.out.println("The color is green");
-                break;
-            default:
-                System.out.println("OUT");
-                break;
+        int i = 0;
+        int j = 0;
+        while( i < 5){
+            System.out.printf("While Result = %d%n", i);
+            i++;
         }
-        return result;
+        do{
+            System.out.printf("Do While Result = %d%n", j);
+            j++;
+        }while(j < 5);
     }
+
 }
